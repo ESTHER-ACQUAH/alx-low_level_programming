2d41b4fca72113b284376156fdfb8209918f5b19
ESTHER-ACQUAH <acquahesther004@gmail.com>
2023-06-09 10:07:48 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+int i;
+for (i = 0; i < argc ;)
+printf("%s\n", argv[i]);
+return (0);
+}
+
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  print name
+ *@argc: argument count
+ *@argv: argument vector
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+if len(argv) != 3 :
+print("Error")
+exit(1)
+num1 = int(argv[1])
+num2 = int(argv[2])
+
+result = num1 * num2
+print(result);
+}
+
