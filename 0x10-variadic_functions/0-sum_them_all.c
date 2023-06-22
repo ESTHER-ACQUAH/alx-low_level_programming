ccc174bc0d3a767a013e3d6c64246b5db4ce7bf5
ESTHER-ACQUAH <acquahesther004@gmail.com>
2023-06-22 13:00:41 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,10 @@ int sum = 0;
 va_list arg_list;
 unsigned int i;
 va_start(arg_list, n);
-if (n == 0) 
+if (n == 0)
 return (0);
 for (i = 0; i < n ; i++)
 sum += va_arg(arg_list, int);
 va_end(arg_list);
 return (sum);
 }
-
