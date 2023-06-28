44bea4555cfe44d979b2e31fc102e4dab65eb1b2
ESTHER-ACQUAH <acquahesther004@gmail.com>
2023-06-28 07:44:55 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,18 +7,17 @@
  *@c: char.
  * Return: Always 0.
  */
-char* create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
-if (size == 0)
-return (NULL);
+	char *str;
+	unsigned int i;
 
-char* arr = (char*) malloc(size * sizeof(char));
-if (arr == NULL)
-return (NULL);
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+		return (NULL);
 
-for (unsigned int i = 0; i < size; i++)
-arr[i] = c;
-
-return (arr);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
 
